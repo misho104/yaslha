2f5934c05b2859e0a51490100aa1c896fad4b2b2
Sho Iwamoto <contact@misho-web.com>
2019-01-06 22:49:48 +0100

    v0.1.0

--- yaslha/__init__.py
+++ yaslha/__init__.py
@@ -8,7 +8,7 @@ from yaslha.core import SLHA, Block, Decay  # noqa: F401
 
 
 __pkgname__ = 'yaslha'
-__version__ = '0.0.6'
+__version__ = '0.1.0'
 __author__ = 'Sho Iwamoto / Misho'
 __license__ = 'MIT'
 
